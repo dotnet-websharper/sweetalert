8816ad2a33aeb649f0fcf9ce651103dbacabb67d
Jand42 <andras.janko@gmail.com>
2017-09-06 16:39:43 +0200

    Rename Zafir packages to WebSharper

--- build-zafir.fsx
+++ build-zafir.fsx
@@ -2,26 +2,26 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("Zafir.SweetAlert")
-        .VersionFrom("Zafir")
+    BuildTool().PackageId("WebSharper.SweetAlert")
+        .VersionFrom("WebSharper")
         .WithFSharpVersion(FSharpVersion.FSharp30)
         .WithFramework(fun f -> f.Net40)
 
 let main =
-    bt.Zafir.Extension("WebSharper.SweetAlert")
+    bt.WebSharper4.Extension("WebSharper.SweetAlert")
         .SourcesFromProject()
         .Embed([])
         .References(fun r -> [])
 
 let tests =
-    bt.Zafir.SiteletWebsite("WebSharper.SweetAlert.Tests")
+    bt.WebSharper4.SiteletWebsite("WebSharper.SweetAlert.Tests")
         .SourcesFromProject()
         .Embed([])
         .References(fun r ->
             [
                 r.Project(main)
-                r.NuGet("Zafir.Testing").Latest(true).Reference()
-                r.NuGet("Zafir.UI.Next").Latest(true).Reference()
+                r.NuGet("WebSharper.Testing").Latest(true).Reference()
+                r.NuGet("WebSharper.UI.Next").Latest(true).Reference()
             ])
 
 bt.Solution [
